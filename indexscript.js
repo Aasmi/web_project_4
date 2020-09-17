0e57e1b00e3831b26bf3a412e71a084e66bc8a80
Aasmi Maam <aasmi.maam@gmail.com>
2020-09-17 00:42:55 -0400

    Submit event and popup values filled from profile, only on opening

--- indexscript.js
+++ indexscript.js
@@ -4,39 +4,35 @@ const editButton = document.querySelector('.profile__edit-button');
 const closeButton = document.querySelector('.popup__close-button');
 const saveButton = document.querySelector('.popup__save-button'); 
 
-const popupContainer = document.querySelector('.popup__container');
+const popupContainer = document.querySelector('.popup__edit-form');
 const popup = document.querySelector('.popup');
 
 const inputName = document.querySelector('.popup__field_type_name');
 const inputAboutme = document.querySelector('.popup__field_type_aboutme');
 
 const profileName = document.querySelector('.profile__name');
-const profileAboutme = document.querySelector('.profile__About-Me');
+const profileAboutme = document.querySelector('.profile__about-me');
 
 
 /*****************Function Definitions */
-function fillProfileValues(){
-    event.preventDefault();    
+function togglePopup(){  
     
+    if (!popup.classList.contains("popup_opened")){
+        inputName.value = profileName.textContent;
+        inputAboutme.value = profileAboutme.textContent;
+    }
+       
+    popup.classList.toggle('popup_opened');
+}
+
+function fillProfileValues(){
+    event.preventDefault();  
+      
     profileName.textContent = inputName.value;
     profileAboutme.textContent = inputAboutme.value;
-
+    
+        
     togglePopup()
-}
-/*
-function fillDefaultValues(){
-    event.preventDefault(); 
-
-    inputName.value = profileName.textContent;
-    inputAboutme.value = profileAboutme.textContent;
-
-    togglePopup()
-}*/
-
-function togglePopup(){
-    inputName.value = profileName.textContent;
-    inputAboutme.value = profileAboutme.textContent;
-    popup.classList.toggle('popup_opened');
 }
 
 /***************************Program */
@@ -45,4 +41,4 @@ editButton.addEventListener('click', togglePopup);
 
 closeButton.addEventListener('click',togglePopup);
 
-saveButton.addEventListener('click',fillProfileValues);
+
